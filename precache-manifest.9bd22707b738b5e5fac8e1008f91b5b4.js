8e0b0b38444630104cd179a6e41aee618d389a0a
unknown <zafarustad123@gmail.com>
2019-10-16 13:22:07 +0530

    Updates

---
+++ precache-manifest.9bd22707b738b5e5fac8e1008f91b5b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea7a7ab38127cd01278cd7ba454d1736",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "5dec81e7ab20c49e4f35",
+    "url": "/pokedex/static/css/main.5c651c5f.chunk.css"
+  },
+  {
+    "revision": "2ed717792fb3d79e9139",
+    "url": "/pokedex/static/js/2.8a57800a.chunk.js"
+  },
+  {
+    "revision": "5dec81e7ab20c49e4f35",
+    "url": "/pokedex/static/js/main.cf876fd8.chunk.js"
+  },
+  {
+    "revision": "df58a94c352d3dee6f0a",
+    "url": "/pokedex/static/js/runtime-main.a1ebc469.js"
+  }
+]);
